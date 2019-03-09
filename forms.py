6dd7dac5e04fb6761ba5355f4395a468fea23efd
Булгаков Сергей <sanyo3102@gmail.com>
2019-03-09 14:49:45 +0300

    login and sign up pages

--- forms.py
+++ forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email
 
 
 class BookUploadForm(FlaskForm):
@@ -9,3 +9,17 @@ class BookUploadForm(FlaskForm):
     title = TextAreaField('Название книги', validators=[DataRequired()])
     file = FileField('Файл не выбран', validators=[FileRequired()])
     submit = SubmitField('Загрузить')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Логин', validators=[DataRequired()])
+    email = StringField('Почта', validators=[Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask, request, render_template, redirect, session, flash
+from flask_login import login_user, LoginManager, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from forms import *
 
 
 app = Flask(__name__)
+app.secret_key = '0bcfb47472328e90fbf26d4ef88d9d90'
 app.config['SECRET_KEY'] = '0bcfb47472328e90fbf26d4ef88d9d90'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 db = SQLAlchemy(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return None
+
 
 # database begins
 class User(db.Model):
@@ -15,10 +27,17 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), unique=False, nullable=False)
+    is_active = True
 
     def __repr__(self):
         return '<User {} {} {}>'.format(
             self.id, self.username, self.email)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.id
 
 
 class Book(db.Model):
@@ -58,10 +77,69 @@ db.create_all()
 # database ends.
 
 
+@app.route('/')
+@app.route('/index')
+def index():
+    if 'username' not in session:
+        return render_template('index.html', username='Гость')
+    else:
+        return render_template('index.html', username=session['username'])
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    user_name1 = form.username.data
+    password1 = form.password.data
+    remember_me = form.remember_me.data
+    if form.validate_on_submit():
+        user = User.query.filter(User.username == user_name1)
+        user = list(user)
+        if user:
+            user = user[0]
+            if user.check_password(password1):
+                login_user(user, remember=remember_me)
+                flash('Вы успешно вошли в систему.')
+                session['username'] = user_name1
+                session['user_id'] = user.id
+                return redirect("/index")
+            else:
+                flash('Неверный логин или пароль.', 'error')
+        else:
+            flash('Неверный логин или пароль.', 'error')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    session.pop('username', 0)
+    session.pop('user_id', 0)
+    return redirect('/')
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
+def sign_up():
+    form = SignUpForm()
+    user_name1 = form.username.data
+    email1 = form.email.data
+    password1 = form.password.data
+    if form.submit.data:
+        user_name_free = False if list(User.query.filter(User.username == user_name1)) else True
+        email_free = False if list(User.query.filter(User.email == email1)) else True
+        if user_name_free and email_free:
+            register_user(user_name1, email1, password1)
+            flash('Вы успешно зарегестрированы.')
+            return redirect("/index")
+        else:
+            flash('Имя пользователя или почта заняты.')
+    return render_template('sign_up.html', title='Регистрация', form=form)
+
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    # if 'username' not in session:
-    #     return redirect('/login')
+    if 'username' not in session:
+        return redirect('/login')
     form = BookUploadForm()
     if form.validate_on_submit():
         upload_book('Adasd', form.title.data, form.author.data, form.file.data)  # TODO: make login page
